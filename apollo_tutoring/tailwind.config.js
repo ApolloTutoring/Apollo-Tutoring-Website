f02833fdff46ae03dd6d565ac0744cc154a1c635
Bhavya Patel <127996743+BhavyaP45@users.noreply.github.com>
2024-07-30 15:06:28 -0400

    Initialize NextJS project and add colours

---
+++ apollo_tutoring/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "standard_white": "#F4F7F5",
+        "standard_navy": "#3247D1",
+        "standard_teal": "#82D4BB",
+        "nav_colour": "#083D77"
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
