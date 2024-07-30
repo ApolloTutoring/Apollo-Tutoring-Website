a474125131c23951867613e3a5429fc12be3cd50
Sajan Paventhan <sajan.paventhan@gmail.com>
2024-07-30 16:17:48 -0400

    import fonts

--- apollo_tutoring/src/app/layout.js
+++ apollo_tutoring/src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Apollo Tutoring Canada",
@@ -12,7 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Oswald:wght@200..700&display=swap" rel="stylesheet"/>
+      </head>
+
+      <body className="">{children}</body>
     </html>
   );
 }
--- apollo_tutoring/tailwind.config.js
+++ apollo_tutoring/tailwind.config.js
@@ -18,6 +18,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        headFont: ['"oswald"', "sans-serif"],
+        paraFont: ['"lato"', "sans-serif"],
+        // Add more custom font families as needed
+      }
     },
   },
   plugins: [],
