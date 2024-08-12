b76c13f87899d0cb66e4f433f75310d915c74970
Bhavya Patel <127996743+BhavyaP45@users.noreply.github.com>
2024-08-12 01:18:18 -0400

    Create cool contact feature

--- apollo_tutoring/src/app/components/contactus.js
+++ apollo_tutoring/src/app/components/contactus.js
@@ -30,32 +30,14 @@ export default function Contact() {
       <div className="bg-standard_navy pt-5 rounded-2xl">
         <h1 className="text-center text-2xl text-white font-headFont">Get in Touch</h1>
         <form onSubmit={onSubmit}>
-          <label className="input input-bordered flex items-center gap-2 m-5 ">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70">
-                <path
-                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input type="text" name="name" className="grow" placeholder="Name" required />
+          <label className="flex items-center gap-2 m-5 ">
+            <input type="text" name="name" className="grow entry" placeholder="Name" required />
           </label>
-          <label className="input input-bordered flex items-center gap-2 m-5 ">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70">
-                <path
-                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                <path
-                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-            </svg>
-            <input type="text" className="grow" placeholder="Email" name="email" required/>
+          <label className="flex items-center gap-2 m-5">
+            <input type="text" className="grow entry" placeholder="Email" name="email" required />
           </label>
           <div className="mx-5">
-              <textarea className=" textarea textarea-bordered w-full" placeholder="Message" name="message" required></textarea>
+              <textarea className=" w-full entry" placeholder="Message" name="message" required></textarea>
               <button className="btn font-paraFont font-bold text-standard_navy my-5 w-full" type="submit">Submit Form</button>
               <span className="text-white mx-auto font-paraFont font-bold">{result}</span>
           </div>  
